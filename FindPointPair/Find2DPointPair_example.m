8209dcaedc05ff627a77fa0d4d8b50d12371b28a
bobobozhou <31420413+bobobozhou@users.noreply.github.com>
2018-04-05 17:36:29 -0400

    Create Find2DPointPair_example.m

---
+++ FindPointPair/Find2DPointPair_example.m
@@ -0,0 +1,29 @@
+clear
+clc
+close all
+
+%% load video and extract image
+vidObj = VideoReader('../Data/stereo1.avi');  % camera_static
+vidHeight = vidObj.Height;
+vidWidth = vidObj.Width;
+s = struct('cdata',zeros(vidHeight,vidWidth,3,'uint8'),...
+    'colormap',[]);
+
+k = 1;
+while hasFrame(vidObj)
+    s(k).cdata = readFrame(vidObj);
+    k = k+1;
+end
+
+img = s(5).cdata;
+img_l = im2double(img(:, 1:end/2, :)); img_r = im2double(img(:, end/2:end, :));
+figure,imshow(img_l, []); figure,imshow(img_r, []);
+
+%% find corresponding points
+[ p_l, p_r ] = Find2DPointPair(img_l, img_r);
+
+figure, imshow(img_l); hold on
+plot(p_l(:,2), p_l(:,1), '.g', 'MarkerSize', 6); hold off
+figure, imshow(img_r); hold on
+plot(p_r(:,2), p_r(:,1), '.g', 'MarkerSize', 6); hold off
+
