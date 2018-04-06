1a10263d864ae7fe3a1979e1d19115b260d132b0
sycmio <suyongchi@126.com>
2018-04-05 21:55:44 -0400

    add some slam functions

---
+++ slam/localize_camera_from_points.m
@@ -0,0 +1,33 @@
+% This function takes 3-D points and their reprojection 2-D points in
+% setero camera, then return the estimation position of stereo camera.
+% Inputs:
+%   P  - Nx3 matrix of 3D coordinates
+%   p1 - Nx2 set of points in left camera
+%   p2 - Nx2 set of points in right camera
+%   K1 - 3x3 camera calibration matrix 1 of left camera
+%   K2 - 3x3 camera calibration matrix 2 of right camera
+%   M_diff - 3x4 Extrinsic matrix from left to right (Right M2- Left M1 = M)
+%   M1_init -3x4 Inital extrinsic matrix value for left camera
+
+% Outputs:
+%   C1 - Nx2 matrix of (x, y) coordinates
+
+function [M1, M2] = localize_camera_from_points(P,p1,p2,K1,K2,M_diff,M1_init)
+R1_init = M1_init(:,1:3);
+t1_init = M1_init(:,4);
+r1_init = rotationMatrixToVector(R1_init);
+x_init = [r1_init;t1_init];
+
+fun = @(x)rodriguesResidual(P,p1,p2,K1,K2,M_diff,x);
+options = optimoptions(@lsqnonlin,'Algorithm','levenberg-marquardt', ...
+     'Display', 'Iter', 'MaxFunctionEvaluations', 120000);
+x = lsqnonlin(fun, x_init, [], [], options);
+
+r1 = x(1:3);
+t1 = x(4:6);
+R1 = rotationVectorToMatrix(r1);
+M1_homo = [R1 t1; 0 0 0 1];
+M2_homo = M1_homo*M_diff;
+M1 = [R1 t1];
+M2 = M2_homo(1:3,:);
+end
---
+++ slam/reconstruct_from_stereo.m
@@ -0,0 +1,31 @@
+% This function uses given stereo camera to reconstruct 3-D points from
+% left and right pictures
+%       C1 - 3x4 Camera Matrix 1
+%       p1 - Nx2 set of points
+%       C2 - 3x4 Camera Matrix 2
+%       p2 - Nx2 set of points
+
+%       P - Nx3 matrix of 3D coordinates
+%       err - scalar of the reprojection error
+function [ P, err ] = reconstruct_from_stereo(C1, p1, C2, p2)
+N = size(p1,1);
+P = zeros(N,3);
+% compute 3-D points
+for i=1:N
+    A = [p1(i,1)*C1(3,:)-C1(1,:);
+       p1(i,2)*C1(3,:)-C1(2,:);
+       p2(i,1)*C2(3,:)-C2(1,:);
+       p2(i,2)*C2(3,:)-C2(2,:)];
+    [V,D] = eig(A);
+    [~,I] = sort(abs(diag(D)));
+    X = V(1:3, I(1))/V(4, I(1));
+    P(i,:) = X';
+end
+% compute the points projection
+project_1_homo = (C1*([P ones(N,1)]'))';
+project_1 = project_1_homo(:,1:2)./project_1_homo(:,3);
+project_2_homo = (C2*([P ones(N,1)]'))';
+project_2 = project_2_homo(:,1:2)./project_2_homo(:,3);
+
+err = norm(reshape([p1-project_1; p2-project_2], [], 1))^2;
+end
