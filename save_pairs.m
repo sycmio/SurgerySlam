c06839c481a39222778d02c455a1be365af23ccd
sycmio <suyongchi@126.com>
2018-04-17 21:44:02 -0400

    update

--- save_pairs.m
+++ save_pairs.m
@@ -2,7 +2,7 @@ addpath(genpath(pwd));
 
 base_path = 'Data/';
 start_frame = 1;
-end_frame = 144;
+end_frame = 108;
 
 %parameters according to the paper
 params.padding = 1.0;         			   % extra area surrounding the target
